ddb7e5001d70f7895b8f00c56f9c1368c3310cdd
wnewstar <192513073@qq.com>
2019-08-28 16:23:54 +0800

    更新

--- main/main.go
+++ main/main.go
@@ -19,13 +19,13 @@ func main() {
         fmt.Println("The HTTP service start at host port %s", os.Args[1])
     } else {
         (router.Route(obj)).Run("0.0.0.0:3000")
-        fmt.Println("The HTTP service start at default host port 0.0.0.0:3000")
+        fmt.Printf("The HTTP service start at default host port 0.0.0.0:3000")
     }
 }
 
 func status() {
     if len(os.Args) >= 3 {
         log.Println(http.ListenAndServe(os.Args[2], nil))
-        fmt.Println("The HTTP status service start at host port %s", os.Args[2])
+        fmt.Printf("The HTTP status service start at host port %s", os.Args[2])
     }
 }
